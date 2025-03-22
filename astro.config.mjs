0b36bbea03a796d835be75ec09e4c02e37717b9a
LightChimera <artemilin347@gmail.com>
2025-03-22 23:11:37 +0300

    Чета сделал и вроде даже починил

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,10 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
-import tailwindcss from '@tailwindcss/vite';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap()],
-
-  vite: {
-    plugins: [tailwindcss()],
-  },
+  integrations: [mdx(), sitemap(), tailwind()],
 });
