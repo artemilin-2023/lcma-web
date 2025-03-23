54042eeb75224a0c3283ea9bc1fe0714cb2a27f6
LightChimera <artemilin347@gmail.com>
2025-03-23 09:03:53 +0300

    Поменять раздел обо мне

--- src/consts.ts
+++ src/consts.ts
@@ -7,5 +7,6 @@ export const SOCIAL_LINKS = {
   mail: 'mailto:chimera@lcma.tech',
   hh: "https://hh.ru/resume/bb1423d1ff0debe8820039ed1f374555367063",
   github: 'https://github.com/artemilin-2023',
-  telegram: 'https://t.me/LightChimera'
+  telegram: 'https://t.me/LightChimera',
+  spotify: 'https://open.spotify.com/user/e93qigv6d4wsr1vgssarjil7p?si=82eda574c7ab46ac'
 };
