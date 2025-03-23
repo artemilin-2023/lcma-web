a7686e5fc320f26e448f9eb5429d918ef74340e4
LightChimera <artemilin347@gmail.com>
2025-03-23 20:15:21 +0300

    Доделать стили для mdx

---
+++ src/components/Alerts/Alerts.js
@@ -0,0 +1,7 @@
+// Экспортируем компоненты для MDX
+export { default as Note } from './Note.astro';
+export { default as Info } from './Info.astro';
+export { default as Warning } from './Warning.astro';
+export { default as Important } from './Important.astro';
+export { default as Success } from './Success.astro';
+export { default as Tip } from './Tip.astro'; 
